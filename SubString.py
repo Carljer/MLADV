0c54e64e1c55bde61f77fe1c8abccbcdf7fa27db
Carljer <carljer@kth.se>
2018-01-16 14:44:20 +0100

    Finding the most used substrings in dataset

---
+++ SubString.py
@@ -0,0 +1,39 @@
+import string
+import itertools
+
+def main(n = 1, dataset = ""):
+	
+	alphabet = string.ascii_lowercase[:] + " "
+
+	allsub_couter = dict()
+
+	#print(alphabet)
+
+	allsub = get_all_substrings(alphabet,n)
+
+	for i in range(len(allsub)):
+
+		allsub_couter[allsub[i]] = 0
+
+	#dicten med data i
+	print(freequency(allsub_couter,dataset,n))
+	#freequency(allsub_couter,dataset,n)
+
+def get_all_substrings(S,n):
+
+	return ["".join(x) for x in itertools.product( S, repeat=n)]
+
+def freequency(allsub_couter, dataset, n):
+
+	for x in range(len(dataset)-1):
+		allsub_couter[dataset[x:x+n]] = allsub_couter[dataset[x:x+n]]+ 1
+
+
+
+
+
+
+	return allsub_couter 
+
+if __name__ == '__main__':
+	main()
