c60a7651228919aa929add21fd502aa8d54b6a67
robertab <robertab@kth.se>
2018-01-17 13:23:30 +0100

    Parse data and filter out non-alphanumerical

--- parse.py
+++ parse.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from lxml import etree
+import re
 
 DIR = "reuters21578"
 
@@ -7,18 +8,22 @@ def read_document(filename):
     with open(DIR + '/' + filename, 'r') as sgm_file:
         soup = BeautifulSoup(sgm_file, 'html.parser')
         earn_counter = 0
-        for content in soup.find_all(lewissplit="TRAIN"):
-            for category in content.find_all('topics'):
-                if category.get_text() == 'earn':
-                    earn_counter += 1
+        result = []
+        for body in soup.find_all('reuters'):
+            b = body.get_text()
+            category = body.find('topics').get_text()
+            if category == 'earn':
+                result.append((re.sub(r'\W+', ' ', b), category))
+            if category == 'acq':
+                result.append((re.sub(r'\W+', ' ', b), category))
+                
+        return result
 
-        print(earn_counter)
 
 def init():
     document = 'reut2-014.sgm'
-    train = read_document(document)
-    # parsed_doc = read_document(document)
-    # for docs in parsed_doc:
-    #     print(docs)
+    data = read_document(document)
+    return data
 
 
+init()
