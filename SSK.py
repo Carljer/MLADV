3b278632856c9e4e6b0eb051288c8ba98b19f2cd
herrmalte <dahlen.christopher@gmail.com>
2018-01-18 12:20:15 +0100

    changed to run Supportvec

--- SSK.py
+++ SSK.py
@@ -6,14 +6,16 @@ pd.set_option('display.max_rows', 1000)
 def feature_mapping_of_substring(s, k):
     return [''.join(x) for x in itertools.combinations(s,k)]
 
-def getPhi(feature_space, doc):
+def getPhi(feature_space, doc, category):
+    dic = dict()
     weight_vec = [None]*len(feature_space)
     i=0
     for feature in feature_space:
         length = get_weight_decay(feature, doc)
         weight_vec[i] = length
         i+=1
-    return weight_vec
+    dic[category] = weight_vec
+    return dic
 
 def get_weight_decay(feature, doc):
     length_counter = 0
@@ -37,14 +39,14 @@ def get_weight_decay(feature, doc):
         j=0
         i+=1
     return 0
-
-def get_phi_matrix(feature_space,docs):
-    phi_mat = []
-#     phi_mat_helper = []
-    for doc in docs:
-#         phi_mat_helper.append(getPhi_helper(feature_space, doc))
-        phi_mat.append(getPhi(feature_space, doc))
-    return phi_mat
+# 
+# def get_phi_matrix(feature_space,doc, category):
+#     phi_mat = []
+# #     phi_mat_helper = []
+#     for doc in docs:
+# #         phi_mat_helper.append(getPhi_helper(feature_space, doc))
+#         phi_mat.append(getPhi(feature_space, doc, category))
+#     return phi_mat
 
 def kernel(phi_mat):
     numerator = np.dot(phi_mat[0], phi_mat[1])
@@ -60,13 +62,8 @@ def get_feature_space(docs,k):
     return set(feature_space)
 
 def start_string_kernel(k, docs, feature_space):
-#     s = "science is organized knowledge"
-#     t = "wisdom is organized life"
-#     docs = [('cat', 'earn'),('car','earn'),('bat','acq'), ('bar','acq')]
-#     k = 2
-#     docs = [s , t]
-#     feature_space = get_feature_space(docs,k)
-#     result_matrix = [[None] * len(docs)]*len(docs)
+    class_A = []
+    class_B = []
     cat_1 = []
     cat_2 = []
     for i in range(len(docs)):
@@ -77,70 +74,65 @@ def start_string_kernel(k, docs, feature_space):
             cat_2.append(docs[i][0])
 
     cat1_matrix = pd.DataFrame([[None] * len(cat_1)]*len(cat_1))
-    cat2_matrix = pd.DataFrame([[None] * len(cat_2)]*len(cat_1))
-    for i in range(len(cat_1)):
-        d1 = cat_1[i]
-        for j in range(len(cat_1)):
-            d2 = cat_1[j]
-            phi_mat = get_phi_matrix(feature_space, [d1,d2])
-            cat1_matrix[j][i] = kernel(phi_mat)
+    cat2_matrix = pd.DataFrame([[None] * len(cat_2)]*len(cat_2))
+    for doc in cat_1:
+        class_A.append(getPhi(feature_space, doc, 'earn'))
+    
+    for doc in cat_2:
+        class_B.append(getPhi(feature_space, doc , 'acq'))
+    
+    data = class_A + class_B
+    print(data)
+#         print(cat2_matrix)
+# 
+#     earn_classification = pd.DataFrame()
+#     earn_classification['means_earn'] = cat1_matrix.mean(axis=1)
+#     earn_classification['means_acq'] = cat2_matrix.mean(axis=1)
+# #     print(cat_1_means)
+# #     print(cat_2_means)
+#     earn_classification['class'] =  earn_classification['means_earn'] >= earn_classification['means_acq']
+#     earn_true_positives = len(earn_classification[earn_classification['class'] == True])
+#     earn_false_positives = len(earn_classification[earn_classification['class'] == False])
+# #     print(earn_positives)
+#     print(earn_classification)
+# 
+# 
+#     cat1_matrix = pd.DataFrame([[None] * len(cat_2)]*len(cat_2))
+#     cat2_matrix = pd.DataFrame([[None] * len(cat_1)]*len(cat_2))
+#     for i in range(len(cat_2)):
+#         d1 = cat_2[i]
+#         for j in range(len(cat_2)):
+#             d2 = cat_2[j]
+#             phi_mat = get_phi_matrix(feature_space, [d1,d2])
+#             cat1_matrix[j][i] = kernel(phi_mat)
 #         print(cat1_matrix)
-    for i in range(len(cat_1)):
-        d1 = cat_1[i]
-        for j in range(len(cat_2)):
-            d2 = cat_2[j]
-            phi_mat = get_phi_matrix(feature_space, [d1,d2])
-            cat2_matrix[j][i] = kernel(phi_mat)
-#         print(cat2_matrix)
-
-    earn_classification = pd.DataFrame()
-    earn_classification['means_earn'] = cat1_matrix.mean(axis=1)
-    earn_classification['means_acq'] = cat2_matrix.mean(axis=1)
-#     print(cat_1_means)
-#     print(cat_2_means)
-    earn_classification['class'] =  earn_classification['means_earn'] >= earn_classification['means_acq']
-    earn_positives = len(earn_classification[earn_classification['class'] == True])
-    earn_negatives = len(earn_classification[earn_classification['class'] == False])
-#     print(earn_positives)
-    print(earn_classification)
-
-
-    cat1_matrix = pd.DataFrame([[None] * len(cat_2)]*len(cat_2))
-    cat2_matrix = pd.DataFrame([[None] * len(cat_1)]*len(cat_2))
-    for i in range(len(cat_2)):
-        d1 = cat_2[i]
-        for j in range(len(cat_2)):
-            d2 = cat_2[j]
-            phi_mat = get_phi_matrix(feature_space, [d1,d2])
-            cat1_matrix[j][i] = kernel(phi_mat)
-        print(cat1_matrix)
-    for i in range(len(cat_2)):
-        d1 = cat_2[i]
-        for j in range(len(cat_1)):
-            d2 = cat_1[j]
-            phi_mat = get_phi_matrix(feature_space, [d1,d2])
-            cat2_matrix[j][i] = kernel(phi_mat)
-#         print(cat2_matrix)
-
-
-#         print(cat2_matrix)
-    acq_classification = pd.DataFrame()
-    acq_classification['means_earn'] = cat1_matrix.mean(axis=1)
-    acq_classification['means_acq'] = cat2_matrix.mean(axis=1)
-#     print(cat_1_means)
-#     print(cat_2_means)
-    acq_classification['class'] =  acq_classification['means_earn'] <= acq_classification['means_acq']
-    acq_positives = len(acq_classification[acq_classification['class'] == True])
-    acq_negatives = len(acq_classification[acq_classification['class'] == False])
-    print(acq_positives)
-    print(acq_negatives)
-    print(earn_positives)
-    print(earn_negatives)
-
-    precision = earn_positives / (acq_negatives + earn_positives)
-    print('precision:',precision)
-    recall = earn_positives / (earn_positives + earn_negatives)
-    print('recall:',recall)
+#     for i in range(len(cat_2)):
+#         d1 = cat_2[i]
+#         for j in range(len(cat_1)):
+#             d2 = cat_1[j]
+#             phi_mat = get_phi_matrix(feature_space, [d1,d2])
+#             cat2_matrix[j][i] = kernel(phi_mat)
+# #         print(cat2_matrix)
+# 
+# 
+# #         print(cat2_matrix)
+#     acq_classification = pd.DataFrame()
+#     acq_classification['means_earn'] = cat1_matrix.mean(axis=1)
+#     acq_classification['means_acq'] = cat2_matrix.mean(axis=1)
+# #     print(cat_1_means)
+# #     print(cat_2_means)
+#     acq_classification['class'] =  acq_classification['means_earn'] <= acq_classification['means_acq']
+#     acq_positives = len(acq_classification[acq_classification['class'] == True])
+#     acq_negatives = len(acq_classification[acq_classification['class'] == False])
+#     print(acq_positives)
+#     print(acq_negatives)
+#     print(earn_true_positives)
+#     print(earn_false_positives)
+# 
+#     precision = earn_true_positives / (earn_true_positives + earn_false_positives)
+#     print('precision:',precision)
+#     recall = earn_positives / (earn_positives + earn_negatives)
+#     print('recall:',recall)
     return 0
 
 
